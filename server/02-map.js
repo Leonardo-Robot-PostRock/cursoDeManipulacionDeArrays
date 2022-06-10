477361452f08ce15ca77d3651565ecdc495e3d9f
Leonardo Puebla <leonardo38361@gmail.com>
2022-06-10 17:42:37 -0300

    Ejemplo de un iterador map

--- server/02-map.js
+++ server/02-map.js
@@ -1,9 +1,12 @@
 const letters = ['a', 'b', 'c'];
 
-const newArray = [];
-for (let i = 0; i < letters.length; i++) {
-    const element = letters[i];
-    newArray.push(element + '++');
-}
+const newArray = letters.map(item => item + '++');
+
+// const newArray = [];
+// for (let i = 0; i < letters.length; i++) {
+//     const element = letters[i];
+//     newArray.push(element + '++');
+// }
 console.log('original', letters);
 console.log('new', newArray);
+
