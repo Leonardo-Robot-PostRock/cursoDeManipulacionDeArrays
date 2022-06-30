52b0b996903fad514962cb751d87d8ac9115c7b1
Leonardo Puebla <leonardo38361@gmail.com>
2022-06-30 20:07:44 -0300

    Filtrar con condicionales

---
+++ server/04-filter.js
@@ -0,0 +1,15 @@
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+/*FILTRAR PALABRAS QUE SEAN MAYORES O IGUALES A 6 LETRAS DE LONGITUD*/
+const newArray = [];
+for (let i = 0; i < words.length; i++) {
+    const item = words[i];
+    if (item.length >= 6) {
+        newArray.push(item);
+    }
+}
+console.log('newArray: ', newArray);
+console.log('original: ', words);
+
+const rta = words.filter(item => item.length >= 6);
+console.log('rta: ', rta);
+console.log('original: ', words);
