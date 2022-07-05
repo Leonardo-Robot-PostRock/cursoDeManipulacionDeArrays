bc3e49f28de70d9fff7a589d54962827a09df4b5
Leonardo Puebla <leonardo38361@gmail.com>
2022-07-05 15:40:57 -0300

    Mas ejemplos de filter

--- server/04-filter.js
+++ server/04-filter.js
@@ -1,5 +1,5 @@
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-/*FILTRAR PALABRAS QUE SEAN MAYORES O IGUALES A 6 LETRAS DE LONGITUD*/
+/*FILTRAR PALABRAS QUE SEAN MAYORES O IGUALES A 6 LETRAS*/
 const newArray = [];
 for (let i = 0; i < words.length; i++) {
     const item = words[i];
@@ -9,6 +9,8 @@ for (let i = 0; i < words.length; i++) {
 }
 console.log('newArray: ', newArray);
 console.log('original: ', words);
+
+/*______________________________________*/
 
 const rta = words.filter(item => item.length >= 6);
 console.log('rta: ', rta);
@@ -35,8 +37,21 @@ const orders = [
         total: 240,
         delivered: true,
     },
+    {
+        customerName: 'Nicol',
+        total: 100,
+        delivered: false
+    }
 ]
 
-const rta3 = orders.filter(item => item.delivered)
+const rta3 = orders.filter(item => item.delivered && item.total >= 100);
 
 console.log('rta3: ', rta3);
+
+const search = (query) => {
+    return orders.filter(item => {
+        return item.customerName.includes(query);
+    })
+}
+
+console.log(search('Nico'));
