c1eaa1bb47ad9bef4c0015a60b8d4302925383a0
Leonardo Puebla <leonardo38361@gmail.com>
2022-07-13 16:16:30 -0300

    Se realiza ejercicio de overlapping, utilizando some

--- server/07-some.js
+++ server/07-some.js
@@ -44,3 +44,45 @@ const orders = [
 
 const rta3 = orders.some(item => item.delivered);
 console.log('rta3', rta3);
+
+//Manejo de fechas
+
+const dates = [
+    {
+        startDate: new Date(2022, 1, 1, 10), //01/01/2022 10:00am
+        endDate: new Date(2022, 1, 1, 11),
+        title: "Cita de trabajo"
+    },
+    {
+        startDate: new Date(2022, 1, 1, 15),
+        endDate: new Date(2022, 1, 1, 15, 30), //01/01/2022 15:30 or 3:30pm
+        title: "Cita con mi jefe"
+    },
+    {
+        startDate: new Date(2022, 1, 1, 20),
+        endDate: new Date(2022, 1, 1, 21),
+        title: "Cena"
+    }
+]
+
+const newAppointment = {
+    startDate: new Date(2022, 1, 1, 8),
+    endDate: new Date(2022, 1, 1, 9, 30)
+}
+/*Installa librería de funciones para manejo de interposición de fechas de #https://date-fns.org/ */
+var areIntervalsOverlapping = require('date-fns/areIntervalsOverlapping')
+
+const isOverlap = (newDate) => {
+    return dates.some(date => {
+        return areIntervalsOverlapping(
+            {
+                start: date.startDate, end: date.endDate
+            },
+            {
+                start: newDate.startDate, end: newDate.endDate
+            }
+        )
+    })
+}
+
+console.log('isOverlap:', isOverlap(newAppointment));
