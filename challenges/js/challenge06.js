f584447ae1b001b4c4f63513cd08176bdbf6261e
Leonardo Puebla <leonardo38361@gmail.com>
2022-07-13 22:42:56 -0300

    Challenge con every.

---
+++ challenges/js/challenge06.js
@@ -0,0 +1,22 @@
+// En este desafío tienes un array de números y debes retornar true si dentro de los números de ese array todos los números son pares.
+
+// La solución debería tener un input y output como los siguientes:
+
+// solution([2, 4, 6, 8, 10]);
+// solution([1, 3, 5, 7, 10, 11]);
+// solution([1, 3, 5]);
+
+// Output
+
+// true 
+// false
+// false}
+
+function solution(numbers) {
+    const rta = numbers.every(number => number % 2 === 0);
+    return rta;
+}
+
+console.log(solution([2, 4, 6, 8, 10]));
+console.log(solution([1, 3, 5, 7, 10, 11]));
+console.log(solution([1, 3, 5]));
